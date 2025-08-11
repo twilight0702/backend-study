78ab2828c1767b34ebc9cdc0e7506c1f911f006b
Twilight <2475911580@qq.com>
2025-08-12 00:58:09 +0800

    2025-08-12 00:58:09

---
+++ springboot-demo/src/main/java/com/twilight/springbootdemo/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.twilight.springbootdemo;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class RedisConfig {
+    @Bean
+
+}
